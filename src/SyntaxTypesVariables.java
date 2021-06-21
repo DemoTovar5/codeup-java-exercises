8c117743dbf83d7cc40e715d06c7539e94883c97
DemoTovar5 <demetrio.tovar5@gmail.com>
2021-06-21 16:45:44 -0500

    java practice

---
+++ src/SyntaxTypesVariables.java
@@ -0,0 +1,39 @@
+//public class SyntaxTypesVariables {
+//
+//    public static void main(String[] args) {
+//
+////        byte myFirstNumber = 127;
+////        byte mySecondNumber = 1;
+//        short myFirstNumber = 32094;
+//        short mySecondNumber = 1;
+//        myFirstNumber += mySecondNumber;
+//        System.out.println(myFirstNumber);
+//        // Bytes only go up to 127, if it goes over 127, it becomes an (Integer).
+//        // (Integer) is a data type in Java.
+//    }
+//}
+
+import java.util.Locale;
+
+public class SyntaxTypesVariables {
+
+    public static void main(String[] args) {
+
+//        System.out.println("Hello");
+
+//        String myName = "Demo";
+//        System.out.println(myName + ", how are you?");
+
+        var isNiceDay = true;
+
+        if (isNiceDay) {
+            System.out.println("What a nice day it is");
+        } else {
+            System.out.println("Ehh...");
+        }
+    }
+}
+
+// every character in Java has an equivalent numerical value.
+
+
