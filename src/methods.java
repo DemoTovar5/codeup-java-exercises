574bc4d6d72133c92569d9ca65571ed009acae2c
DemoTovar5 <demetrio.tovar5@gmail.com>
2021-06-28 17:17:33 -0500

    Methods

--- src/methods.java
+++ src/methods.java
@@ -2,28 +2,43 @@ import java.util.Scanner;
 public class methods {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the name of a basketball team: ");
-        String basketballTeam = sc.next();
-        System.out.println(print(basketballTeam));
-        System.out.println(len(print(basketballTeam)));
+//        System.out.println("Enter the name of a basketball team: ");
+//        String basketballTeam = sc.next();
+          System.out.println("Enter your full name: ");
+          String userName = sc.nextLine();
+
+          String[] array = userName.split(" ");
+//        System.out.println(Teams(basketballTeam));
+//        System.out.println(lengthOf(Teams(basketballTeam)));
+        System.out.println(Names(userName));
+        System.out.println(Names(array[0],array[1]));
+    }
+//
+//    public static String Teams(String basketballTeam){
+//        return basketballTeam + " is the best team.";
+//    }
+//
+//   public static String lengthOf(String basketballTeam){
+//        return "The length of the entire statement is : " + basketballTeam.length();
+//   }
+
+
+    public static String Names(String userName) {
+       return "Hello " + userName;
     }
 
-    public static String print (String basketballTeam){
-        return basketballTeam + " is the best team.";
+    public static String Names(String firstName, String lastName){
+        return "Hello " + firstName + " " + lastName;
     }
-
-   public static String len(String basketballTeam){
-        return "The length of the entire statement is : " + basketballTeam.length();
-   }
-
-
-
-
 
 
 
 }
 
+//    TODO: invoke a method that accepts (and returns) the following:
+//        - accepts: your first name and your last name. returns: A
+//        string that reads: "Hello Laura Ruiz-Roehrs (replace with your
+//        first and last)";
 
     //    TODO: create a new method that accepts the name of a basketball
 //     team (ex: the spurs) and returns a String that says
