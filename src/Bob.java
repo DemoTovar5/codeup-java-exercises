50fc79f20639a343a0f3e6137cd06a4133c26580
DemoTovar5 <demetrio.tovar5@gmail.com>
2021-06-28 14:01:00 -0500

    Changes

--- src/Bob.java
+++ src/Bob.java
@@ -1,2 +1,25 @@
+import java.util.Scanner;
 public class Bob {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        sc.useDelimiter("\n");
+        String userInput = sc.next();
+        if(userInput.endsWith("?")){
+            System.out.println("sure");
+        }else if(userInput.endsWith("!")){
+            System.out.println("Woah, Chill out!");
+        }else if(userInput.equals("")){
+            System.out.println("Fine,be that way");
+        }else{
+            System.out.println("Whatever");
+        }
+
+
+
+
+
+
+
+    }
 }
