5e6f65f323a179ce921985383dd03e4c98ecc3ca
DemoTovar5 <demetrio.tovar5@gmail.com>
2021-07-01 12:00:01 -0500

    practice

---
+++ src/oop/animals/AquaMammal.java
@@ -0,0 +1,28 @@
+package oop.animals;
+
+public class AquaMammal extends MarineAnimal{
+
+    private int maxTimeSubmerged;
+
+    public AquaMammal(boolean canSwim, int length, int finCount, boolean isSaltwater, String name, boolean hasScales, int maxTimeSubmerged) {
+
+        super(canSwim, length, finCount, isSaltwater, name);
+        this.maxTimeSubmerged = maxTimeSubmerged;
+    }
+
+    public AquaMammal(String name){
+        super(name);
+    }
+
+    public AquaMammal(){
+    }
+
+    public int getMaxTimeSubmerged() {
+        return maxTimeSubmerged;
+    }
+
+    public void setMaxTimeSubmerged(int maxTimeSubmerged) {
+        this.maxTimeSubmerged = maxTimeSubmerged;
+    }
+
+}
