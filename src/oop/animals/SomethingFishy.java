c92cfbb78a0b80c8751266c68229c9c8125efd44
DemoTovar5 <demetrio.tovar5@gmail.com>
2021-07-01 15:31:43 -0500

    Fish

--- src/oop/animals/SomethingFishy.java
+++ src/oop/animals/SomethingFishy.java
@@ -3,7 +3,7 @@ package oop.animals;
 public class SomethingFishy {
     public static void main(String[] args) {
 
-        MarineAnimal marineAnimal = new AquaMammal("Blue Whale");
+        var marineAnimal = new AquaMammal("Blue Whale");
 
         System.out.println(marineAnimal.getName());
 
