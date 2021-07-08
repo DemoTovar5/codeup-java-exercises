e45f87f2d6746eefbc7a525f5a272a5cd28482aa
DemoTovar5 <demetrio.tovar5@gmail.com>
2021-07-08 14:49:08 -0500

    Arrays

--- src/collections/ArrayLecture.java
+++ src/collections/ArrayLecture.java
@@ -5,7 +5,13 @@ import java.util.Scanner;
 
 public class ArrayLecture {
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(getNameArray()));
+//        System.out.println(Arrays.toString(getNameArr()));
+
+
+        String[] personOneNames = getNameArray();
+        String[] PersonTwoNames = getNameArray();
+
+//        System.out.println(Arrays.equals(personOneNames,personTwoNames));
     }
 
 //    public static int[] getIntArray(){
@@ -23,22 +29,25 @@ public class ArrayLecture {
 //        return intArray;
 //    }
 
-public static String[] getNameArray(){
+    public static String[] getNameArray() {
         Scanner sc = new Scanner(System.in);
-    System.out.println("How many names do you want to enter?");
+        System.out.println("How many names do you want to enter?");
 
-    int namesLength = sc.nextInt();
-    String[] namesArr = new String[namesLength];
+        int namesLength = sc.nextInt();
+        String[] namesArr = new String[namesLength];
 
-    for (int i = 0; i < namesLength; i++){
-        namesArr[i] = sc.next();
+        for (int i = 0; i < namesLength; i++) {
+            namesArr[i] = sc.next();
+        }
+
+        return namesArr;
     }
 
-    return  namesArr;
-}
-
-
-
+    public static String[] getNameArr() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name please: ");
+        return sc.nextLine().split(" ");
+    }
 
 
 }
