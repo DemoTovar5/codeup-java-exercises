0a153588a78034c5e5fff7227b41ccded3654aec
DemoTovar5 <demetrio.tovar5@gmail.com>
2021-07-09 16:06:36 -0500

    student

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -3,5 +3,6 @@ package grades;
 public class GradesApplication {
     public static void main(String[] args) {
 
+
     }
 }
--- src/grades/student.java
+++ src/grades/student.java
@@ -1,6 +1,47 @@
 package grades;
+import java.util.ArrayList;
 
 public class student {
 
 
+    // methods
+
+    private String name;
+    private Integer grade;
+
+
+    // constructor
+
+    public student(String name) {
+        this.name = name;
+    }
+
+//    public student(Integer grade){
+//        this.grade = new ArrayList<>();
+//
+//
+//    }
+
+
+
+
+
+// setters and getters
+
+
+    public String getName() {
+        return name;
+    }
+
+//    // adds the given grade to the grades property
+//    public void addGrade(int grade) {
+//    }
+
+//    // returns the average of the students grades
+//    public double getGradeAverage() {
+//
+//    }
+
 }
+
+
