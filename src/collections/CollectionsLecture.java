9bb0fb7670bb32f363df66c82c08f47a14a8a607
DemoTovar5 <demetrio.tovar5@gmail.com>
2021-07-09 11:14:59 -0500

    collections

---
+++ src/collections/CollectionsLecture.java
@@ -0,0 +1,26 @@
+package collections;
+
+import java.util.ArrayList;
+
+public class CollectionsLecture {
+
+    public static void main(String[] args) {
+        ArrayList<String> stringArr = new ArrayList<>();
+
+        // add first element
+        stringArr.add("Demo");
+        System.out.printf("Current ArrayList size: %s\n", stringArr.size());
+        System.out.println(stringArr);
+        System.out.println(stringArr.hashCode());
+
+       // add second element
+        stringArr.add("Raul");
+        System.out.printf("Current ArrayList size: %s\n", stringArr.size());
+        System.out.println(stringArr);
+        System.out.println(stringArr.hashCode());
+
+        stringArr.remove(1);
+        System.out.println(System.identityHashCode(stringArr));
+
+    }
+}
--- src/oop/TernaryTester.java
+++ src/oop/TernaryTester.java
@@ -1,18 +1,47 @@
 package oop;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class TernaryTester {
-    public static void main(String[] args) {
-//        String str= "My name Is Demo";
-
+    public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        String userInput = sc.nextLine();
 
-        for (int i = 0; i < userInput.length(); i++) {
+        System.out.println("Enter the size of the array: ");
+        int size = sc.nextInt();
 
-            if(Character.isUpperCase(userInput.charAt(i))){
-                System.out.println(userInput.charAt(i));
-            }
+        String myArray[] = new String[size];
+        System.out.println("Enter your favorite vacation spots: ");
+        for (int i = 0; i < size; i++) {
+            myArray[i] = sc.next();
         }
+
+        System.out.println(Arrays.toString(myArray));
+        ArrayList<String> myList = new ArrayList<String>(Arrays.asList(myArray));
+
+        System.out.println("Enter one more favorite vacation spot: ");
+        String element = sc.next();
+        myList.add(element);
+        myArray = myList.toArray(myArray);
+
+        System.out.println(Arrays.toString(myArray));
     }
 }
+
+//        String userInput = sc.nextLine();
+//
+//        for (int i = 0; i < userInput.length(); i++) {
+//
+//            if(Character.isUpperCase(userInput.charAt(i))){
+//                System.out.println(userInput.charAt(i));
+//            }
+//        }}
+
+
+// TODO: Create a method which will return a String[] containing the user's favorite vacation spots
+//  -> Each time the user inputs a new vacation spot, reset the array to be one element longer in length
+//  -> Then, add the old elements
+//  -> Finally, add the new element
+//  -> When the user decides to finish inputting spots, return the String[].
+//  -> sout out the returned value!
