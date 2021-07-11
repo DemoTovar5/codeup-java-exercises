8429d3e95b63bd958e7ff8399d67600911cbc9c9
DemoTovar5 <demetrio.tovar5@gmail.com>
2021-07-10 21:06:28 -0500

    changes

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -1,20 +1,22 @@
+import java.util.Random;
+
 public class ServerNameGenerator {
     public static void main(String[] args) {
 
-//        public static String[] animals = {"dog","cat","pig","donkey","pikachu","bird","lizard","llama","kangaroo","fish"};
-//
-//        public static String[] adjective = {"outstanding", "useful", "woebegone", "outrageous", "fragile", "fumbling", "sad","happy", "strong", "quick"};
-//
-//
 
+    }
 
-//        public static String randomElement(String[] ){
-//
-//            int i = (int) (Math.random() * array.length);
-//            return array[i];
+        public static String randomElement(String[]) {
+
+            String[] animals = {"dog","cat","pig","donkey","pikachu","bird","lizard","llama","kangaroo","fish"};
+
+            String[] adjective = {"outstanding", "useful", "woebegone", "outrageous", "fragile", "fumbling", "sad","happy", "strong", "quick"};
+
+            Random r = new Random();
+            int randomNumber=r.nextInt(arr.length);
+            System.out.println(arr[randomNumber]);
 
         }
-
     }
 
 
